a0186a59bcebe97e6d96559cb5e5eca28a7fd5bb
pouya-barzegar <poooooyabarzegar@gmail.com>
2017-08-10 14:39:45 +0430

    add logger for api requests

--- apifetch.py
+++ apifetch.py
@@ -1,13 +1,21 @@
 import requests as req
+import logging
 
 # from telegbot import logger
 
 # baseurl = "http://sunbyteit.com/"
 
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename='./telegbot.log',
+                    format=LOG_FORMAT,
+                    filemode='w',
+                    level=logging.DEBUG)
+
+logger = logging.getLogger()
 
 def fetch_json(base, route):
     product = req.get(base + route)
-    # logger.info("a request to the api was sent")
+    logger.info("a request to the api was sent")
     return product.json()
 
     reply_markup = build_menu(button_list, n_cols=len(cat_names))
