5d64e6b1f8c5cb412df04df3e3b74ed20dafbe06
pouya-barzegar <poooooyabarzegar@gmail.com>
2017-08-16 16:10:11 +0430

    redis necessary or excess?

--- dbase.py
+++ dbase.py
@@ -2,9 +2,21 @@ import redis
 import time
 import apifetch
 
-global database
-database = 2
+# global database
+# database = 2
 
+
+def start_redis(db=2):
+    """
+
+    Args:
+        db
+    Returns:
+        redis_obj
+    """
+
+    rd = redis.StrictRedis(host="localhost", port=6379, db=db)
+    return rd
 
 def update_from_website(field):
     """
@@ -15,7 +27,7 @@ def update_from_website(field):
     Returns:
         status
     """
-    rd = redis.StrictRedis(host="localhost", port=6379, db=database)
+    rd = start_redis()
     last_update = rd.get("Last_update_time")
     if last_update["hour"] - time.time().tm_hour < "2":
        return "NO"
@@ -26,7 +38,7 @@ def update_from_website(field):
         rd.set("Last_update_time", last_update)
         return "OK"
     elif field == "products":
-        products = apifetch.fetch_json("http://www.sunbyteit.com:8000/api/", "categories")
+        products = apifetch.fetch_json("http://www.sunbyteit.com:8000/api/", "products")
         rd.set("products", values=products)
         return "OK"
     last_update = []
